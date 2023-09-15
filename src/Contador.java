bc223202fc3a74c8e8641c9d600525d428f47821
William Sousa <williamrsousa94@hotmail.com>
2023-09-14 23:04:22 -0300

    Mensagem da exceção e validação da exceção

--- src/Contador.java
+++ src/Contador.java
@@ -14,12 +14,15 @@ public class Contador {
 		
 		}catch (ParametrosInvalidosException exception) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		
+		if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+        }
 		int contagem = parametroDois - parametroUm;
 		//realizar o for para imprimir os números com base na variável contagem
 	}
